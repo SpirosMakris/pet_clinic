f32a84bb1a2e5fcfb83f697071e4f5daabcbaf7d
Makris Spiros <makris.spiros@gmail.com>
2023-11-21 21:07:28 +0200

    Update foreign key constraint in pets table migration

--- database/migrations/2023_11_14_101031_create_pets_table.php
+++ database/migrations/2023_11_14_101031_create_pets_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
 
             $table->foreignId('owner_id')
                 ->nullable()
-                ->constrained('owners');
+                ->constrained('users');
 
             $table->timestamps();
         });
